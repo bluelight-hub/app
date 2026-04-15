06e8fd8128c00defcf41e3c5f3f3ce72d4842726
Ruben Vitt <dev@rubeen.dev>
2026-04-15 15:33:24 +0200

    ✨(frontend): Add FunkspruchComposer mit Hotkey-Submit

--- packages/frontend/src/features/funkverkehr/ui/organisms/index.ts
+++ packages/frontend/src/features/funkverkehr/ui/organisms/index.ts
@@ -1,3 +1,4 @@
+export * from './FunkspruchComposer.organism';
 export * from './KanalEditDrawer.organism';
 export * from './KanalplanTable.organism';
 export * from './ZuordnungsManager.organism';
